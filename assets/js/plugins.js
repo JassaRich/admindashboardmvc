90194594be7ecaeb7cde4539ca3089c0320b1684
ajay@magnetontech <ajay@magnetontech.com>
2024-04-06 16:04:05 +0530

    upalod files

---
+++ assets/js/plugins.js
@@ -0,0 +1,1 @@
+document.querySelectorAll("[toast-list]")&&document.writeln("<script src='assets/js/toastify.js'><\/script>"),document.querySelectorAll("[data-provider]")&&document.writeln("<script src='assets/js/flatpickr.min.js'><\/script>"),document.querySelectorAll("[data-choices]")&&document.writeln("<script src='assets/js/choices.min.js'><\/script>");
